6fde78dfc78508fd19f5e2f086e691690c863f0d
GUILHERME GRAIFF DE CAMPOS <00001124145114sp@aluno.educacao.sp.gov.br>
2026-05-07 16:45:17 -0300

    carrossel

---
+++ Frontend/carrossel.js
@@ -0,0 +1,36 @@
+const carousel = document.getElementById('carrossel');
+
+const images = carousel.querySelectorAll('img');
+let index = 0;
+
+
+
+ function autoSlide() {
+index++;
+
+// Se chegar na última imagem, volta para a primeira
+
+if (index >= images.length) {
+index = 0;
+}
+
+// Calcula o deslocamento
+const offset = -index * 100;
+
+carousel.style.transform = `translateX(${offset}%)`;
+}
+
+// Define o intervalo (3000ms = 3 segundos)
+
+setInterval(autoSlide, 2000);
+
+ function moveSlide (direction) {
+     const slides = document.querySelectorAll('.carousel-inner img');
+
+     slides[currentSlide].classList.remove('active');
+
+currentSlide = (currentSlide + direction + slides.length) % slides.length;
+
+slides [currentSlide].classList.add('active');
+
+}
